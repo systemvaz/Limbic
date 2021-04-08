0802446e9b189a4547a3409273a0909ca425bd36
systemvaz <systemvaz@outlook.com>
2021-04-08 13:05:03 +0800

    Refactoring

--- FaceDetect/detect.py
+++ FaceDetect/detect.py
@@ -4,70 +4,101 @@ import os
 import numpy as np
 import tensorflow as tf
 
-cascPath = os.curdir + '/models/haarcascade_frontalface_default.xml'
-modelPath = os.curdir + '/models/EmoDetect_ResNet164v2_model.068.h5'
+def get_model_paths():
+    cascPath = os.curdir + '/models/haarcascade_frontalface_default.xml'
+    modelPath = os.curdir + '/models/EmoDetect_ResNet164v2_model.068.h5'
+    
+    return cascPath, modelPath
 
-angry_emj = cv2.imread(os.curdir + '/emojis/angry.png')
-disgust_emj = cv2.imread(os.curdir + '/emojis/disgust.png')
-fear_emj = cv2.imread(os.curdir + '/emojis/fear.png')
-happy_emj = cv2.imread(os.curdir + '/emojis/happy.png')
-sad_emj = cv2.imread(os.curdir + '/emojis/sad.png')
-surprised_emj = cv2.imread(os.curdir + '/emojis/surprised.png')
-neutral_emj = cv2.imread(os.curdir + '/emojis/neutral.png')
 
-classes_dict = {0:'Angry', 1:'Disgust', 2:'Fear', 3:'Happy', 
-                4:'Sad', 5:'Surprised', 6:'Neutral'}
+def get_emoji_paths():
+    angry_emj = cv2.imread(os.curdir + '/emojis/angry.png')
+    disgust_emj = cv2.imread(os.curdir + '/emojis/disgust.png')
+    fear_emj = cv2.imread(os.curdir + '/emojis/fear.png')
+    happy_emj = cv2.imread(os.curdir + '/emojis/happy.png')
+    sad_emj = cv2.imread(os.curdir + '/emojis/sad.png')
+    surprised_emj = cv2.imread(os.curdir + '/emojis/surprised.png')
+    neutral_emj = cv2.imread(os.curdir + '/emojis/neutral.png')
+    
+    return angry_emj, disgust_emj, fear_emj,\
+           happy_emj, sad_emj, surprised_emj, neutral_emj
 
-emoji_dict = {0:angry_emj, 1:disgust_emj, 2:fear_emj, 3:happy_emj, 
-              4:sad_emj, 5:surprised_emj, 6:neutral_emj}
 
-model = tf.keras.models.load_model(modelPath, compile=True)
-faceCascade = cv2.CascadeClassifier(cascPath)
-video_capture = cv2.VideoCapture(0)
+def get_dicts():
+    classes_dict = {0:'Angry', 1:'Disgust', 2:'Fear', 3:'Happy', 
+                    4:'Sad', 5:'Surprised', 6:'Neutral'}
+    emoji_dict   = {0:angry_emj, 1:disgust_emj, 2:fear_emj, 3:happy_emj, 
+                    4:sad_emj, 5:surprised_emj, 6:neutral_emj}
 
-while True:
-    # Capture frame-by-frame
-    ret, frame = video_capture.read()
+    return classes_dict, emoji_dict
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(48, 48),
-        flags=cv2.CASCADE_SCALE_IMAGE
-    )
+def capture_and_detect(video_capture, faceCascade, model):
+    while True:
+        # Capture frame-by-frame
+        ret, frame = video_capture.read()
 
-    # Draw a rectangle around the faces
-    for (x, y, w, h) in faces:
-        crop_img = frame[y:y+h, x:x+w]
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Crop face and predict expression
-    try:
-        crop_img = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
-        crop_img = crop_img.astype('float32') / 255
-        crop_img = cv2.resize(crop_img, (48, 48))
-        crop_img = crop_img.reshape(1,48,48,1)
-        expression = model.predict(crop_img)
-        expression = int(np.argmax(expression, axis = 1))
-        overlay = emoji_dict.get(expression, neutral_emj)
-        result = classes_dict.get(expression, "Don't know")
-    except:
-        print("Problem getting facial expression...")
+        faces = faceCascade.detectMultiScale(gray,
+                                             scaleFactor=1.1,
+                                             minNeighbors=5,
+                                             minSize=(48, 48),
+                                             flags=cv2.CASCADE_SCALE_IMAGE)
 
-    # Add emoji and text to top left corner
-    rows, cols, channels = overlay.shape
-    frame[0:rows, 0:cols ] = overlay
-    cv2.putText(frame, result, (rows+30, cols//2), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (209, 80, 0, 255), 3)
+        # Draw a rectangle around the faces
+        for (x, y, w, h) in faces:
+            crop_img = frame[y:y+h, x:x+w]
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-    # Display the resulting frame
-    cv2.imshow('Video', frame)
+        # Crop face and predict expression
+        try:
+            crop_img = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
+            crop_img = crop_img.astype('float32') / 255
+            crop_img = cv2.resize(crop_img, (48, 48))
+            crop_img = crop_img.reshape(1,48,48,1)
+            expression = model.predict(crop_img)
+            expression = int(np.argmax(expression, axis = 1))
+            overlay = emoji_dict.get(expression, neutral_emj)
+            result = classes_dict.get(expression, "Don't know")
+        except:
+            print("Problem getting facial expression...")
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        # Add emoji and text to top left corner
+        rows, cols, channels = overlay.shape
+        frame[0:rows, 0:cols ] = overlay
+        cv2.putText(frame, result, (rows+30, cols//2), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (209, 80, 0, 255), 3)
 
-# When everything is done, release the capture
-video_capture.release()
-cv2.destroyAllWindows()
+        # Display the resulting frame
+        cv2.imshow('Video', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+
+def main():
+    # Get filepath declarations
+    cascPath, modelPath = get_model_paths()
+
+    angry_emj, disgust_emj, fear_emj,\
+    happy_emj, sad_emj, surprised_emj, neutral_emj = get_emoji_paths()
+
+    classes_dict, emoji_dict = get_dicts(angry_emj, disgust_emj, fear_emj,
+                                        happy_emj, sad_emj, surprised_emj, neutral_emj)
+
+    # Load model and face detection algo
+    model = tf.keras.models.load_model(modelPath, compile=True)
+    faceCascade = cv2.CascadeClassifier(cascPath)
+    video_capture = cv2.VideoCapture(0)
+
+    # Main video capture loop
+    capture_and_detect(video_capture, faceCascade, model)
+
+    # When everything is done, release the capture
+    video_capture.release()
+    cv2.destroyAllWindows()
+    
+
+
+if __name__ == "__main__":
+    main()
---
+++ FaceDetect/model.py
@@ -0,0 +1,100 @@
+import tensorflow as tf
+from keras.regularizers import l2
+from keras import backend as K
+from keras.models import Model
+
+#ResNet layer
+def resnet_layer(inputs,
+                 num_filters=16,
+                 kernel_size=3,
+                 strides=1,
+                 activation='relu',
+                 batch_normalization=True,
+                 conv_first=True):
+
+    conv = tf.keras.layers.Conv2D(num_filters,
+                                  kernel_size=kernel_size,
+                                  strides=strides,
+                                  padding='same',
+                                  kernel_initializer='he_normal',
+                                  kernel_regularizer=l2(1e-4))
+
+    x = inputs
+
+    if conv_first:
+        x = conv(x)
+        if batch_normalization:
+            x = tf.keras.layers.BatchNormalization()(x)
+        if activation is not None:
+            x = tf.keras.layers.Activation(activation)(x)
+    else:
+        if batch_normalization:
+            x = tf.keras.layers.BatchNormalization()(x)
+        if activation is not None:
+            x = tf.keras.layers.Activation(activation)(x)
+        x = conv(x)
+
+    return x
+#End ResNet layer
+
+#Define our ResNet model
+def resnet(input_shape, depth, num_classes=NUM_CLASSES):
+    num_filters_in = 16
+    num_res_blocks = int((depth -2) / 9)
+    inputs = tf.keras.Input(shape=input_shape)
+    x = resnet_layer(inputs=inputs, num_filters=num_filters_in, conv_first=True)
+
+    for stage in range(3):
+        for res_block in range(num_res_blocks):
+            activation = 'relu'
+            batch_normalization = True
+            strides = 1
+            if stage == 0:
+                num_filters_out = num_filters_in * 4
+                if res_block == 0:
+                    activation = None
+                    batch_normalization = False
+            else:
+                num_filters_out = num_filters_in * 2
+                if res_block == 0:
+                    strides = 2
+
+            y = resnet_layer(inputs=x,
+                             num_filters=num_filters_in,
+                             kernel_size=1,
+                             strides=strides,
+                             activation=activation,
+                             batch_normalization=batch_normalization,
+                             conv_first=False)
+
+            y = resnet_layer(inputs=y,
+                             num_filters=num_filters_in,
+                             conv_first=False)
+
+            y = resnet_layer(inputs=y,
+                             num_filters=num_filters_out,
+                             kernel_size=1,
+                             conv_first=False)
+
+            if res_block == 0:
+                x = resnet_layer(inputs=x,
+                                 num_filters=num_filters_out,
+                                 kernel_size=1,
+                                 strides=strides,
+                                 activation=None,
+                                 batch_normalization=False)
+
+            x = tf.keras.layers.add([x, y])
+
+        num_filters_in = num_filters_out
+
+        x = tf.keras.layers.BatchNormalization()(x)
+        x = tf.keras.layers.Activation('relu')(x)
+        x = tf.keras.layers.AveragePooling2D(pool_size=8)(x)
+        y = tf.keras.layers.Flatten()(x)
+        outputs = tf.keras.layers.Dense(num_classes, activation='softmax', kernel_initializer='he_normal')(y)
+
+        model = tf.keras.Model(inputs=inputs, outputs=outputs)
+
+        return model
+#End of ResNet definition
