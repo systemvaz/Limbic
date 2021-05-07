5c23421d8354dfe99ed749f67f06653c9078bbe7
systemvaz <systemvaz@outlook.com>
2021-05-07 13:15:10 +0800

    refector

--- TextSentiment/preprocess_data.py
+++ TextSentiment/preprocess_data.py
@@ -1,7 +1,6 @@
 import numpy as np
 import os
 
-# data = pd.read_csv(, usecols=[0, 5])
 data = np.genfromtxt(os.curdir + '/data/twitter_all.csv', delimiter='",', usecols=(0,5), dtype=str)
 data = np.char.strip(data, chars='"')
 data[data == '4'] = '1'
